c428f94af9ecf1648cb241b69c16ba00f83cacec
antony502189-max <antony502189@gmail.com>
2026-07-24 22:09:59 +0300

    Load hardened mobile styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './rental-emphasis.css'
+import './mobile-app-v2-hardening.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
