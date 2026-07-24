f2a75d95e24baae8a71535724374a9026b903f77
antony502189-max <antony502189@gmail.com>
2026-07-24 22:43:48 +0300

    Apply mobile hardening styles after component styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './rental-emphasis.css'
-import './mobile-app-v2-hardening.css'
 import App from './App.tsx'
+import './mobile-app-v2-hardening.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
