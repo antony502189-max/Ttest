fcdcfbea61cc4cb3625c91ee09afe309e9585a2e
Anton Gameza <antony502189@gmail.com>
2026-09-18 14:02:56 +0300

    Use canonical publication street field in publish-smart-address-autocomplete.spec.ts

--- tests/publish-smart-address-autocomplete.spec.ts
+++ tests/publish-smart-address-autocomplete.spec.ts
@@ -73,7 +73,7 @@ test('Calle José Espronceda 20 shows a suggestion inline and selecting it fixes
 
   await expect(page.getByLabel('Municipio')).toHaveValue('Adeje')
   await expect(page.getByLabel('Zona o barrio')).toHaveValue('Armeñime')
-  await expect(page.getByLabel('Calle')).toHaveValue('Calle José Espronceda 20')
+  await expect(page.locator('#publish-street')).toHaveValue('Calle José Espronceda 20')
   await expect(page.getByLabel('Código postal')).toHaveValue('38678')
   await expect.poll(() => page.evaluate(() => window.__googleMapsTestLastMap?.getZoom())).toBe(18)
   await expect.poll(() => page.evaluate(() => {
